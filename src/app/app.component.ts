205efac3d3698e3127fce4dab7a73f4033385b21
“Padmapriya” <kairawitty@gmail.com>
2023-10-30 00:03:08 +0530

    Commiting myProduct changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'studybuddy';
+  constructor(private http: HttpClient) {}
+
+  fetchData() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts/1/comments')
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
